4a47727bf803dd464b3a88cb584f86b508df12ed
Xu Jianglin <fantasyshow88@163.com>
2017-10-09 22:30:59 +0800

    add futuretask

--- demo/src/com/statestreet/demo/java/thread/basic/TraditionalThread.java
+++ demo/src/com/statestreet/demo/java/thread/basic/TraditionalThread.java
@@ -1,8 +1,13 @@
 package com.statestreet.demo.java.thread.basic;
 
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 public class TraditionalThread {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		
 		//define a thread with overwritten run method
 		Thread t1 = new Thread() {
@@ -41,5 +46,18 @@ public class TraditionalThread {
 		
 		t3.start();//runnable~
 		
+		//future task implements interface runnable
+		FutureTask<Integer> futureTask = new FutureTask<>(new Callable<Integer>() {
+
+			@Override
+			public Integer call() throws Exception {
+				Thread.sleep(new Random().nextInt(3000));
+				return new Random().nextInt(100);
+			}
+		});
+		
+		Thread t4 = new Thread(futureTask);
+		t4.start();
+		System.out.println(futureTask.get());
 	}
 }
