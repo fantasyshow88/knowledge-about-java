777fbba389a56d072b1d06a37a80bb80405eb868
Xu Jianglin <fantasyshow88@163.com>
2018-03-19 21:54:28 +0800

    test spring

--- demo/src/com/statestreet/demo/test/DeadLockTest.java
+++ demo/src/com/statestreet/demo/test/DeadLockTest.java
@@ -3,6 +3,7 @@ package com.statestreet.demo.test;
 public class DeadLockTest {
 	public static Object o1 = new Object();
 	public static Object o2 = new Object();
+	public static Object o3 = new Object();
 	
 	
 	public static void main(String[] args) {
@@ -32,6 +33,24 @@ public class DeadLockTest {
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
+					synchronized (o3) {
+						
+					}
+					
+					
+				}
+			}
+		}).start();
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				synchronized (o3) {
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 					synchronized (o1) {
 						
 					}
@@ -39,7 +58,7 @@ public class DeadLockTest {
 					
 				}
 			}
-		}).start();;
+		}).start();
 		
 	}
 }
