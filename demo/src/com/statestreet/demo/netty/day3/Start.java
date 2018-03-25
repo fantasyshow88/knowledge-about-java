0c9475234f315664a47803cfed38c72132d1c72a
Xu Jianglin <fantasyshow88@163.com>
2018-03-25 14:55:20 +0800

    netty

---
+++ demo/src/com/statestreet/demo/netty/day3/Start.java
@@ -0,0 +1,28 @@
+package com.statestreet.demo.netty.day3;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import com.statestreet.demo.netty.day3.pool.NioSelectorRunnablePool;
+/**
+ * 启动函数
+ *
+ */
+public class Start {
+
+	public static void main(String[] args) {
+
+		
+		//初始化线程
+		NioSelectorRunnablePool nioSelectorRunnablePool = new NioSelectorRunnablePool(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+		
+		//获取服务类
+		ServerBootstrap bootstrap = new ServerBootstrap(nioSelectorRunnablePool);
+		
+		//绑定端口
+		bootstrap.bind(new InetSocketAddress(10101));
+		
+		System.out.println("start");
+	}
+
+}
---
+++ demo/src/com/statestreet/demo/netty/day3/pool/Boss.java
@@ -0,0 +1,15 @@
+package com.statestreet.demo.netty.day3.pool;
+
+import java.nio.channels.ServerSocketChannel;
+/**
+ * boss接口
+ *
+ */
+public interface Boss {
+	
+	/**
+	 * 加入一个新的ServerSocket
+	 * @param serverChannel
+	 */
+	public void registerAcceptChannelTask(ServerSocketChannel serverChannel);
+}
