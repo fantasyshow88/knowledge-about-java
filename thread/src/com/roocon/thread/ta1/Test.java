f3094581fbd3f22eddbe9ad9b24a6823e625478b
Xu Jianglin <fantasyshow88@163.com>
2019-01-13 11:15:22 +0800

    structure change

--- thread/src/com/roocon/thread/ta1/Test.java
+++ thread/src/com/roocon/thread/ta1/Test.java
@@ -1,5 +1,6 @@
 package com.roocon.thread.ta1;
 
+import java.io.IOException;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -19,66 +20,9 @@ public class Test {
 		lock.unlock();
 	}
 	
-	public static void main(String[] args) {
-		
-		Test t = new Test();
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				while(true)
-				t.a();
-			}
-		}).start();
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				while(true)
-				t.a();
-			}
-		}).start();
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				while(true)
-				t.a();
-			}
-		}).start();
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				while(true)
-				t.a();
-			}
-		}).start();
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				while(true)
-				t.a();
-			}
-		}).start();
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				while(true)
-				t.a();
-			}
-		}).start();
-		
-		
-		
+	public static void main(String[] args) throws IOException {
+		System.in.read();
+		System.out.println("EBD");
 	}
 
 }
